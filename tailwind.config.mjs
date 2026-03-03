10a3e5c7fda1d2917b14fff55b84a65eb5640272
AbelTomato <abeltomato@gmail.com>
2026-03-03 14:33:14 +0800

    chore: 修改了tailwindcss的配置，从而优化了博文的排版；上传了一篇博文；添加了一名朋友

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "selector", // 必须是字符串 'class'，不是数组
+  darkMode: "selector",
   theme: { extend: {} },
-  plugins: [],
+  plugins: [typography],
 };
