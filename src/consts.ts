18f76573b5a6e786cb3e283985fae0fafcaa181b
AbelTomato <abelmiaotomato@gmail.com>
2026-05-15 13:10:55 +0800

    docs: 修改了主页描述

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "AbelTomato's Blog";
-export const SITE_DESCRIPTION =
-  "AbelTomato's world,I wrote some inexplicable content.";
+export const SITE_DESCRIPTION = "Time tries all, but TLE.";
 export const AUTHOR = "AbelTomato";
