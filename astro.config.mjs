f01f08dfff0135508e1d04235ae2f1c2bf657482
AbelTomato <abelmiaotomato@gmail.com>
2026-06-22 19:27:34 +0800

    chore: update Astro site for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://AbelTomato.github.io",
+  site: "https://abel-tomato-github-io.vercel.app",
   base: "/",
   integrations: [
     mdx({
